babad410860edf92e8910647a9a429a28c366e6d
shujiejune <jieniu.june@tutanota.com>
2026-01-02 11:56:10 -0800

    initial commit

---
+++ src/test/java/org/example/shopify/ShopifyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shopify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
