4e3952060b8571c895f28452193f23450a2abfa1
shujiejune <jieniu.june@tutanota.com>
2026-01-08 22:10:08 -0800

    feat(AOP): add AfterThrowing advice for exceptions

--- src/main/java/org/example/shopify/AOP/LoggingAspect.java
+++ src/main/java/org/example/shopify/AOP/LoggingAspect.java
@@ -1,6 +1,8 @@
 package org.example.shopify.AOP;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -8,8 +10,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingAspect {
-
-    // Target all methods in the Service package
+    // Advice 1: Measure execution time for all service methods
     @Around("execution(* org.example.shopify.Service.*.*(..))")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
@@ -20,5 +21,13 @@ public class LoggingAspect {
         System.out.println(joinPoint.getSignature() + " executed in " + executionTime + "ms");
 
         return proceed;
+    }
+
+    // Advice 2: Log when an exception is thrown in the Service layer
+    @AfterThrowing(pointcut = "execution(* org.example.shopify.Service.*.*(..))", throwing = "ex")
+    public void logServiceException(JoinPoint joinPoint, Exception ex) {
+        // Catch IllegalOrderStateException and ResourceNotFoundException
+        System.out.println("ALERT: Exception in " + joinPoint.getSignature().getName() +
+                " | Message: " + ex.getMessage());
     }
 }
